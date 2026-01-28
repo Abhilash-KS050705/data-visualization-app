fb79f22d45113fb1710abd200f86b2587de7c54c
Abhilash <abhilashks0505@gmail.com>
2026-01-28 12:51:58 +0530

    backend working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/")
+      .then(res => setMessage(res.data.message))
+      .catch(() => setMessage("Backend not connected"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Data Analyst & Visualization App</h1>
+      <p>{message}</p>
     </div>
   );
 }
